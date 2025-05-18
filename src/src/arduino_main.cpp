7edc33082c97fac47611a138049f83948a59b768
isomo <jiahaoxiang2000@gmail.com>
2025-05-18 18:26:05 +0800

    Add UART support for STM32L475VG with custom TX/RX pin definitions

--- src/src/arduino_main.cpp
+++ src/src/arduino_main.cpp
@@ -40,6 +40,16 @@
 #ifdef LWC_PLATFORM_l475vg
 // stm32l4xx_hal_uart.h, these is support by the STM32CubeL4 HAL library
 UART_HandleTypeDef huart1;
+
+// Define the serial pins explicitly for the STM32L475VG board
+#define SERIAL_TX_PIN PA9
+#define SERIAL_RX_PIN PA10
+const PinMap PinMap_UART_TX[] = {
+    {PA_9, USART1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF7_USART1)}, // USB_OTG_FS_VBUS
+};
+const PinMap PinMap_UART_RX[] = {
+    {PA_10, USART1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF7_USART1)}, // USB_OTG_FS_ID
+};
 #endif
 
 void setup()
@@ -48,21 +58,26 @@ void setup()
 // Don't use IO for size experiments to reduce the overall size of the binary
 #ifndef LWC_EXPERIMENT_SIZE
 
-  // initialize LED digital pin as an output and open the serial port
-  #ifdef LWC_PLATFORM_l475vg
+// initialize LED digital pin as an output and open the serial port
+#ifdef LWC_PLATFORM_l475vg
   pinMode(PE9, OUTPUT);
+
+  // Initialize Serial with specific pins PA9 (TX) and PA10 (RX)
+  // The setPins method must be called before begin()
+  Serial.setRx(SERIAL_RX_PIN);
+  Serial.setTx(SERIAL_TX_PIN);
   Serial.begin(9600);
   Serial.println();
-   stop_watch(3);
-  #elif LCB_PLATFORM_esp32s3
+  stop_watch(3);
+#elif LCB_PLATFORM_esp32s3
   pinMode(GPIO_NUM_1, OUTPUT);
   Serial0.begin(9600);
   Serial0.println();
   stop_watch(5);
-  #endif
+#endif
 #endif
 
-  int ret = do_experiments();
+  //   int ret = do_experiments();
 
 #ifndef LWC_EXPERIMENT_SIZE
   SOUT << "# LCB exit " << SENDL;
@@ -71,16 +86,16 @@ void setup()
 
 void loop()
 {
-  #ifdef LCB_PLATFORM_esp32s3
-    // let the eps32s3 board blink
-    gpio_set_level(GPIO_NUM_1, 1);
-    delay(1000);
-    gpio_set_level(GPIO_NUM_1, 0);
-    delay(1000);
+#ifdef LCB_PLATFORM_esp32s3
+  // let the eps32s3 board blink
+  gpio_set_level(GPIO_NUM_1, 1);
+  delay(1000);
+  gpio_set_level(GPIO_NUM_1, 0);
+  delay(1000);
 
-  #endif
-  // turn the LED on (HIGH is the voltage level)
-  #ifdef LWC_PLATFORM_l475vg
+#endif
+// turn the LED on (HIGH is the voltage level)
+#ifdef LWC_PLATFORM_l475vg
   digitalWrite(PE9, HIGH);
   // wait for a second
   delay(1000);
@@ -88,5 +103,5 @@ void loop()
   digitalWrite(PE9, LOW);
   // wait for a second
   delay(1000);
-  #endif
+#endif
 }
